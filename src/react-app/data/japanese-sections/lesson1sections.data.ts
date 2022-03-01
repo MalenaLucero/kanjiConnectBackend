f6ec1a5bb442d1fe21ab58ee2b9fcdd7f91ca937
MalenaLucero <luceromalena@hotmail.com>
2022-03-01 10:11:03 -0300

    class links added

--- src/react-app/data/japanese-sections/lesson1sections.data.ts
+++ src/react-app/data/japanese-sections/lesson1sections.data.ts
@@ -3,25 +3,25 @@ import { Section } from "src/react-app/entities/section.entity";
 export const JAPANESE_LESSON_1_SECTIONS: Section[] = [
     {
         _id: 101,
-        link: '',
+        link: 'https://docs.google.com/document/d/1j-VuNtxvSSHxEOp0LT-Cp5DJbG5tKTCbDiPC2EDzQss/edit#heading=h.antkp0wmu2as',
         title: '1 オクトーバーフェスト',
         _sourceId: 1,
         state: 'done'
     }, {
         _id: 102,
-        link: 'https://www.youtube.com/watch?v=_ExoqS3s17Y&list=PLavQwENTsEBVkrFMHDYs-EatjsxyoNNO-',
+        link: 'https://docs.google.com/document/d/1j-VuNtxvSSHxEOp0LT-Cp5DJbG5tKTCbDiPC2EDzQss/edit#heading=h.6il4ljfi42ei',
         title: '01.コース紹介｜初心者にわかりやすいようなコースを作りました',
         _sourceId: 2,
         state: 'done'
     }, {
         _id: 103,
-        link: 'https://www.youtube.com/watch?v=ZJh1HkLiKfI&list=PLavQwENTsEBVkrFMHDYs-EatjsxyoNNO-&index=2',
+        link: 'https://docs.google.com/document/d/1j-VuNtxvSSHxEOp0LT-Cp5DJbG5tKTCbDiPC2EDzQss/edit#heading=h.5jca8vtcbjk2',
         title: '02.JavaScriptとは？｜JavaScriptの特徴やできることなどをわかりやすく解説',
         _sourceId: 2,
         state: 'done'
     }, {
         _id: 104,
-        link: 'https://www.youtube.com/watch?v=C7AdxmIe42M&list=PLavQwENTsEBVkrFMHDYs-EatjsxyoNNO-&index=5',
+        link: 'https://docs.google.com/document/d/1j-VuNtxvSSHxEOp0LT-Cp5DJbG5tKTCbDiPC2EDzQss/edit#heading=h.12pbkdxfxds0',
         title: '04.プログラムの3つの基本構造｜プログラムの動きはシンプル。動きは3つだけ',
         _sourceId: 2,
         state: 'done'
